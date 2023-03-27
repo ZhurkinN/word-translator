e667341bee262afa2b67d77029f4670d89fdf123
ZhurkinN <zhurkin236@gmail.com>
2023-03-27 13:05:44 +0300

    Initial commit

---
+++ src/test/java/ru/zhurkin/translatortask/TranslatorTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zhurkin.translatortask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
